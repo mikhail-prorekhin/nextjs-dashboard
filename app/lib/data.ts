a07325207e781596e6f8787bee0df14755c13f3a
Mikhail Prorekhin <mikhail.prorekhin@gmail.com>
2023-12-08 11:21:02 +1100

    remove unstable_noStore

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -35,7 +35,7 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-    noStore();
+    //noStore();
     //  await new Promise((resolve) => setTimeout(resolve, 5000));
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -57,7 +57,7 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
   try {
-    noStore();
+    // //noStore();
     await new Promise((resolve) => setTimeout(resolve, 5000));
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
@@ -98,7 +98,7 @@ export async function fetchFilteredInvoices(
   currentPage: number,
 ) {
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
-  noStore();
+  //noStore();
   try {
     const invoices = await sql<InvoicesTable>`
       SELECT
@@ -129,7 +129,7 @@ export async function fetchFilteredInvoices(
 }
 
 export async function fetchInvoicesPages(query: string) {
-  noStore();
+  //noStore();
   try {
     const count = await sql`SELECT COUNT(*)
     FROM invoices
@@ -151,7 +151,7 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
-  noStore();
+  //noStore();
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -177,7 +177,7 @@ export async function fetchInvoiceById(id: string) {
 }
 
 export async function fetchCustomers() {
-  noStore();
+  //noStore();
   try {
     const data = await sql<CustomerField>`
       SELECT
@@ -196,7 +196,7 @@ export async function fetchCustomers() {
 }
 
 export async function fetchFilteredCustomers(query: string) {
-  noStore();
+  //noStore();
   try {
     const data = await sql<CustomersTableType>`
 		SELECT
@@ -230,7 +230,7 @@ export async function fetchFilteredCustomers(query: string) {
 }
 
 export async function getUser(email: string) {
-  noStore();
+  //noStore();
   try {
     const user = await sql`SELECT * FROM users WHERE email=${email}`;
     return user.rows[0] as User;
